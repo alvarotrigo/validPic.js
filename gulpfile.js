f4fb16163e49a9ab6e3dc7b49910b97c1cc44f6f
Alvaro Trigo López <atrigol@gmail.com>
2016-04-13 20:48:05 +0100

    Added minified versions

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ var gp_rename = require('gulp-rename');
 var gp_sourcemaps = require('gulp-sourcemaps');
 var gp_uglify = require('gulp-uglify');
 var gp_minify_css = require('gulp-minify-css');
-var gp_concat = require('gulp-concat');
 
 gulp.task('js', function() {
     gulp.src([
